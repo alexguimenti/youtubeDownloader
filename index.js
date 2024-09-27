4df996bcf79de545080d9c5c03b4c70d739f38c8
alexguimenti <alexguimenti@gmail.com>
2024-09-27 16:12:45 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { spawn } = require('child_process');
+const ytdl = require('ytdl-core'); // Importa o pacote ytdl-core
 const app = express();
 const PORT = process.env.PORT || 3030;
 
@@ -14,8 +14,8 @@ app.listen(PORT, () => {
 app.get('/download', async (req, res) => {
   const videoUrl = req.query.url;
 
-  if (!videoUrl) {
-    return res.status(400).send('URL do vídeo é necessária');
+  if (!videoUrl || !ytdl.validateURL(videoUrl)) {
+    return res.status(400).send('URL do vídeo é necessária ou inválida.');
   }
 
   try {
@@ -26,25 +26,16 @@ app.get('/download', async (req, res) => {
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
 
-    // Use spawn para executar o yt-dlp diretamente
-    const ytDlpProcess = spawn('yt-dlp', ['-o', '-', '-f', 'best', videoUrl]);
-
-    // Pipe o stdout para a resposta
-    ytDlpProcess.stdout.pipe(res);
-
-    // Capturar e exibir quaisquer erros do yt-dlp
-    ytDlpProcess.stderr.on('data', (data) => {
-      console.error(`Erro do yt-dlp: ${data}`);
-    });
-
-    ytDlpProcess.on('close', (code) => {
-      console.log(`Processo yt-dlp encerrado com o código ${code}`);
-    });
-
-    ytDlpProcess.on('error', (err) => {
-      console.error('Erro ao baixar o vídeo:', err);
-      res.status(500).send('Erro ao baixar o vídeo');
-    });
+    // Usar ytdl-core para baixar o vídeo
+    ytdl(videoUrl, { format: 'mp4' })
+      .pipe(res)
+      .on('finish', () => {
+        console.log('Download concluído.');
+      })
+      .on('error', (err) => {
+        console.error('Erro ao baixar o vídeo:', err);
+        res.status(500).send('Erro ao baixar o vídeo');
+      });
 
   } catch (error) {
     console.error('Erro ao processar a requisição:', error);
